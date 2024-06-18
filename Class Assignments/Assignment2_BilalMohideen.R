84d6659b6c838214f2a6fec1f854649108acc70f
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-06-18 14:12:18 -0400

    Converting "answer" from character data into numeric form

--- Class Assignments/Assignment2_BilalMohideen.R
+++ Class Assignments/Assignment2_BilalMohideen.R
@@ -4,3 +4,9 @@
 #Prompt the user to enter a three digit positive number
 
 answer <- readline(prompt = "Please enter a three digit positive number: ")
+
+#Converting "answer" from character data into numeric form
+
+answer <- as.numeric(answer)
+
+
