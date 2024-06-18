a08c6ed9d026a6f097090444f18cd530de763cda
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-06-18 14:08:48 -0400

    Creating the prompt for the user to enter a three digit number

--- Class Assignments/Assignment2_BilalMohideen.R
+++ Class Assignments/Assignment2_BilalMohideen.R
@@ -1,3 +1,6 @@
 #Assignment 2 - Narcissistic Numbers
 #Bilal Mohideen - BTC1855H
 
+#Prompt the user to enter a three digit positive number
+
+answer <- readline(prompt = "Please enter a three digit positive number: ")
