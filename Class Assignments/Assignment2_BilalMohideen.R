184235f6116befa0d018dba147e75e092d550b57
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-06-18 23:51:11 -0400

    Defined new variable ("answer_num") as "answer" in numeric form. Improved readability of Error messages.

--- Class Assignments/Assignment2_BilalMohideen.R
+++ Class Assignments/Assignment2_BilalMohideen.R
@@ -9,25 +9,27 @@ answer <- readline(prompt = "Please enter a three digit positive number: ")
 #Separate number into individual digits with "strsplit" function
 #After cubing each of the digits, the sum of of the cubes is calculated
 
-digits <- as.numeric(strsplit(as.character(answer),"")[[1]])
+digits <- as.numeric(strsplit((answer),"")[[1]])
 
 digits_cubed <- (digits^3)
 
 digits_sum <- (sum(digits_cubed))
 
-answer <- as.numeric(answer)
+answer_num <- as.numeric(answer)
 
 #Checking if the number is valid (three digits, positive)
 
-if (is.na(answer)){
-  print("Error: Entry is not a number.")
-} else if (answer < 0){
-  print("Error: Entry is a negative number.")
-} else if (nchar(as.character(answer)) < 3){
-  print("Error: Entry is less than three digits.")
-} else if (digits_sum == answer){
+if (is.na(answer_num)){
+  print(paste("Error:", answer, "is not a number."))
+} else if (nchar(as.character(answer_num)) < 3){
+  print(paste("Error:", answer, "is less than three digits."))
+} else if (nchar(as.character(answer_num)) > 3){
+  print(paste("Error:",answer, "is more than three digits."))
+} else if (answer_num < 0){
+  print(paste("Error:", answer, "is a negative number."))
+} else if (digits_sum == answer_num){
   print(paste(answer,"is an Armstrong number."))
-} else if (digits_sum != answer){
+} else if (digits_sum != answer_num){
   print(paste(answer,"is not an Armstrong number."))
 }
 
