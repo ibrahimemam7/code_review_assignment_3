312f3be827597d2d1cecbae34779e8b32db7edfb
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-06-18 22:45:22 -0400

    Added steps at the beginning of the code to split the number into digits, cube the digits, and take the sum of the cubes. I also started writing "if else" conditions to verify that the number is valid.

--- Class Assignments/Assignment2_BilalMohideen.R
+++ Class Assignments/Assignment2_BilalMohideen.R
@@ -6,7 +6,21 @@
 answer <- readline(prompt = "Please enter a three digit positive number: ")
 
 #Converting "answer" from character data into numeric form
+#Separate number into individual digits with "strsplit" function
+#After cubing each of the digits, the sum of of the cubes is calculated
 
-answer <- as.numeric(answer)
+digits <- as.numeric(strsplit((answer),"")[[1]])
 
+digits_cubed <- (digits^3)
 
+digits_sum <- (sum(digits_cubed))
+
+#Checking if the number is valid (three digits, positive)
+
+if (is.na(answer)){
+  print("Error: Entry is not a number.")
+} else if (answer < 0){
+  print("Error: Entry is a negative number.")
+} else if (length(answer) < 3){
+  print("Error: Entry is less than three digits.")
+} 
