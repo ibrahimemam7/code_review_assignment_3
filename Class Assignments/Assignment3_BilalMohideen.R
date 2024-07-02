9814ca94acc52e4912d25b1f2036203141174e41
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-07-01 22:04:29 -0400

    Created word list with random foods, added list as a text file to the working directory

--- Class Assignments/Assignment3_BilalMohideen.R
+++ Class Assignments/Assignment3_BilalMohideen.R
@@ -1,3 +1,12 @@
 #Assignment 3 - Hangman
 #Bilal Mohideen - BTC1855H
+#R Version 2024.04.1+748 (2024.04.1+748)
+
+#ensure that word bank (random_foods.txt)
+#is saved in working directory
+#set to the appropriate working directory
+setwd("/Users/bilalmohideen/Desktop/MBiotech - UofT/BTC1855H/BM_BTC1855H/Class Assignments")
+
+#reading word list (food themed in this case)
+readLines("random_foods.txt")
 
